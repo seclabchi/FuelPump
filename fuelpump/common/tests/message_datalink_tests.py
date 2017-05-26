3ff68c16d3ca7c350b89ec7633ae1f9879d3e006
Paul Zaremba <zaremba@tonekids.com>
2017-05-26 18:17:09 -0500

    More datalink tests

--- fuelpump/common/tests/message_datalink_tests.py
+++ fuelpump/common/tests/message_datalink_tests.py
@@ -10,10 +10,9 @@ from fuelpump.common.message_datalink import MessageDatalink
 
 
 class MessageDatalinkTests(unittest.TestCase):
-    
-    message_reference = None
-    
+        
     def __init__(self, *args, **kwargs):
+        self.message_reference = None
         super(MessageDatalinkTests, self).__init__(*args, **kwargs)
 
     def print_enc(self, msg):
@@ -34,15 +33,22 @@ class MessageDatalinkTests(unittest.TestCase):
         msg = MessageFactory.get_text("Hello from the \x10 MessageDatalinkTests test_encode test!!!\x10\x10")
         self.print_enc(msg)
         
-    def test_local_decode(self):
+    def test_local_decode_ping_req(self):
         test_msg_source = MessageFactory.get_ping_req()
         self.message_reference = test_msg_source.get_bytes()
         
         enc_msg = MessageDatalink.encode(test_msg_source)
         
         datalink = MessageDatalink()
-        decoded_msg = datalink.decode(enc_msg, self.decode_callback)
+        datalink.decode(enc_msg, self.decode_callback)
         
+    def test_local_decode_text(self):
+        test_msg_source = MessageFactory.get_text("Hello \x10 \x10 \x02 \x03 from the purposefully \x03 \x02 \x10 weirdly-crafted text message datalink test.")
+        self.message_reference = test_msg_source.get_bytes()
+        enc_msg = MessageDatalink.encode(test_msg_source)
+        
+        datalink = MessageDatalink()
+        datalink.decode(enc_msg, self.decode_callback)
         
         
 if __name__ == "__main__":
