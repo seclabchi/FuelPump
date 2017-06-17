63e948c66958c31b811bff73a04b0f0f10e122a1
Paul Zaremba <zaremba@tonekids.com>
2017-06-17 09:56:37 -0500

    Adding message engine

---
+++ fuelpump/common/message_engine.py
@@ -0,0 +1,17 @@
+'''
+Created on Jun 8, 2017
+
+@author: zaremba
+'''
+
+class MessageEngine(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
